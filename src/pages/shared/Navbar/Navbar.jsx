0a44306386ceaaf590af4bfc543438bda6a090d8
mim2041 <suraiyamim338@gmail.com>
2023-05-27 12:13:41 +0600

    photo URL field added

--- src/pages/shared/Navbar/Navbar.jsx
+++ src/pages/shared/Navbar/Navbar.jsx
@@ -20,6 +20,7 @@ const Navbar = () => {
         {
           user ? 
           <>
+            <span>{user?.displayName}</span>
             <button onClick={handleLogOut} className="btn btn-ghost">LogOut</button>
           </>
            : <li><Link to='/login'>Login</Link></li>
