0d7fb0b0bc4be6facc00a64c3a6e8518097cc7cc
mim2041 <suraiyamim338@gmail.com>
2023-05-31 21:17:00 +0600

    Dashboard added

--- src/pages/Dashboard/MyCart/MyCart.jsx
+++ src/pages/Dashboard/MyCart/MyCart.jsx
@@ -12,6 +12,7 @@ const MyCart = () => {
             <Helmet>
                 <title>Bistro Boss | My Cart</title>
             </Helmet>
+            
             <div className="uppercase">
                 <h3 className="text-3xl">Total Items: {cart.length}</h3>
                 <h3 className="text-3xl">Total Price: ${total}</h3>
