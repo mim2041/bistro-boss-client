4ac3eb6892dd2d732ced68bfc7eb89333409843c
mim2041 <suraiyamim338@gmail.com>
2023-06-01 22:07:28 +0600

    foods delete from cart successful

--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -2,10 +2,10 @@ import { useQuery } from '@tanstack/react-query'
 import { useContext } from 'react';
 import { AuthContext } from '../providers/AuthProvider';
 
-const useCart = (email) => {
+const useCart = () => {
     const {user} = useContext(AuthContext);
 
-    const {  refetch, data: cart = [], error } = useQuery({
+    const {  refetch, data: cart = [] } = useQuery({
         queryKey: ['carts', user?.email],
         queryFn: async() => {
             const response = await fetch(`http://localhost:5000/carts?email=${user.email}`)
